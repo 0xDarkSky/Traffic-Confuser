c2728b499ddafa7fec6d2957dcc6e59f3b6069db
QL0R <84932430+QL0R@users.noreply.github.com>
2021-07-29 00:20:26 +0300

    Update traffic-confuser.py

--- traffic-confuser.py
+++ traffic-confuser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 import requests as req
 import os
 import time
@@ -6,6 +7,7 @@ import os.path
 from os import path
 import sys
 import random
+from datetime import datetime
 
 LICENSE = """
 MIT License
@@ -35,7 +37,6 @@ Contributors: QL0R
 Licensed under the MIT License
 """
 
-
 logo = """
 ░██████╗░██╗░░░░░░█████╗░██████╗░  ████████╗██████╗░░█████╗░███████╗███████╗██╗░█████╗░
 ██╔═══██╗██║░░░░░██╔══██╗██╔══██╗  ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝██║██╔══██╗
@@ -60,28 +61,54 @@ def super_type(b):
         sys.stdout.flush()
         time.sleep(0.03)
 
+def root_check():
+    sudo_check = os.geteuid() == 0
+    if sudo_check == True:
+       pass
+    else:
+       super_type("Run script as root! (Only for the first time)\n")
+       exit()
+
 def web_check():
     if path.exists("sample.txt"):
        pass
     else:
        super_type("File sample.txt is missing. Creating file...")
        Q = open("sample.txt", "a")
-       Q.write("https://www.reddit.com\nhttps://www.github.com\nhttps://www.youtube.com\nhttps://www.facebook.com\nhttps://www.google.com\nhttps://www.weather.com\nhttps://www.cnn.com\nhttps://www.twitter.com\nhttps://www.wikipedia.org\nhttps://www.quora.com\n")
+       Q.write("https://whatsapp.com\nhttps://vimeo.com\nhttps://mozilla.org\nhttps://linkedin.com\nhttps://microsoft.com\nhttps://www.blogger.com\nhttps://apple.com\nhttps://www.reddit.com\nhttps://www.github.com\nhttps://www.youtube.com\nhttps://www.facebook.com\nhttps://www.google.com\nhttps://www.weather.com\nhttps://www.cnn.com\nhttps://www.twitter.com\nhttps://www.wikipedia.org\nhttps://www.quora.com\n")
        Q.close()
        super_type("\n\nFile created.\n")
-      
 
+def run_anywhere():
+    super_type("Do you want to copy the file to /usr/local/bin? This will let you run the code from any directory by typing Traffic-Confuser. Don't do it if you're on Windows or Termux because it won't work.")
+    ch = input("\nY/n? ")
+    if "y" == ch.lower():
+        os.system("mv traffic-confuser.py Traffic-Confuser")
+        os.system("cp Traffic-Confuser /usr/local/bin")
+        super_type("Now you can run it anywhere by typing: Traffic-Confuser")
+        time.sleep(2)
+        os.system('cls' if os.name == 'nt' else 'clear')
+    if "n" == ch.lower():
+        pass
+    
 def file_check():
     if path.exists("CodeCheck"):
-       pass
+       x = os.stat("CodeCheck")
+       age = (time.time()-x.st_mtime)
+       if age > 356029.69091153145:
+          super_type("\nYou should check the repository https://github.com/QL0R/Traffic-Confuser, the files might be outdated.\n")
+          time.sleep(2)
+          pass
     else:
        print(LICENSE)
        time.sleep(3)
        os.system('cls' if os.name == 'nt' else 'clear')
-       super_type("License display is a one time thing!\n")
+       root_check()
+       super_type("License check is a one time thing.\n")
        f = open("CodeCheck", "a")
-       f.write("\n-- Please don't delete me, I'm just a simple check for the code, I don't take much space :) --")
+       f.write("\n\n-- Please don't delete me, I'm just a simple check for the Traffic-Confuser.py code, I don't take much space :) --")
        f.close()
+       run_anywhere()
        
 
 def start_status():
@@ -101,20 +128,19 @@ def send_stuff():
          sleepr = random.randint(10,30)
          sleeper = float(sleepr)
          time.sleep(sleeper)
-
+        
 count = 0
 web_check()
 file_check()
 start_status()
+
 while True:
     try:
        count += 1
-       send_stuff() #you can switch to other modes by changing this function to another       
-
+       send_stuff() 
+      
     except KeyboardInterrupt:
-       super_type(f"\nSent {count} requests.\n")
+       super_type(f"\nSent {count} requests.")
        time.sleep(1)
        os.system('cls' if os.name == 'nt' else 'clear')
        exit()
-
-
